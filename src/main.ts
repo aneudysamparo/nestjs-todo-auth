4c4edf2367a63a3b5bda762eaf64681527b4cdce
Aneudys Amparo <aneudys_amparo@bhdleon.com.do>
2019-07-12 08:00:25 -0400

    -Adding Swagger again!

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
-import { DatabaseGuard } from './shared/guards/database.guard';
 
 declare const module: any;
 
@@ -11,7 +10,27 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const hostDomain = AppModule.isDev ? `${AppModule.host}:${AppModule.port}` : AppModule.host;
 
+    const swaggerOptions = new DocumentBuilder()
+        .setTitle('Nest MEAN')
+        .setDescription('API Documentation')
+        .setVersion('1.0.0')
+        .setHost(hostDomain.split('//')[1])
+        .setSchemes(AppModule.isDev ? 'http' : 'https')
+        .setBasePath('/api')
+        .addBearerAuth('Authorization', 'header')
+        .build();
 
+    const swaggerDoc = SwaggerModule.createDocument(app, swaggerOptions);
+
+    SwaggerModule.setup('/api/docs', app, swaggerDoc, {
+        swaggerUrl: `${hostDomain}/api/docs-json`,
+        explorer: true,
+        swaggerOptions: {
+            docExpansion: 'list',
+            filter: true,
+            showRequestDuration: true,
+        },
+    });
 
     if (module.hot) {
         module.hot.accept();
@@ -20,7 +39,6 @@ async function bootstrap() {
 
     app.setGlobalPrefix('api');
     app.useGlobalFilters(new HttpExceptionFilter());
-    app.useGlobalGuards(new DatabaseGuard());
     await app.listen(AppModule.port);
 }
 
